d6f316fd9d0a5564366e010453ed9f4431a6f450
Dan Mills <dmills@qumulo.com>
2025-02-14 21:03:25 -0800

    end of day play

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/evilhamsterman/secret-injector
 
-go 1.23.5
+go 1.24.0
 
 require (
 	github.com/spf13/afero v1.11.0
@@ -23,7 +23,9 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/lmittmann/tint v1.0.7 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
--- main.go
+++ main.go
@@ -1,30 +1,102 @@
 package main
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
 	"fmt"
+	"log/slog"
+	"os"
+	"time"
 
-	"github.com/spf13/afero"
+	"github.com/evilhamsterman/secret-injector/pkg/signals"
+	"github.com/lmittmann/tint"
+	"github.com/mattn/go-isatty"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/selection"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	data := "hello there"
+	log := slog.Default()
+	ctx := signals.SetupSignalHandler()
 
-	hash := sha256.New()
-	hash.Write([]byte(data))
-	hashValue := hash.Sum(nil)
+	config := clientcmd.NewDefaultClientConfigLoadingRules()
+	cfg, err := clientcmd.BuildConfigFromKubeconfigGetter("", config.GetStartingConfig)
+	if err != nil {
+		log.Error("Failed to load kubeconfig", slog.Any("error", err))
+		os.Exit(1)
+	}
 
-	fmt.Printf("Data: %s\n Hash: %s\n", data, hex.EncodeToString(hashValue))
+	log.Info("Successfully loaded kubeconfig file")
 
-	fs := afero.NewOsFs()
+	client, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		log.Error("Failed to create kubernetes client", slog.Any("error", err))
+	}
 
-	f, _ := afero.ReadFile(fs, "playground/test.txt")
+	l, _ := labels.NewRequirement("coder.com/project", selection.In, []string{"playground", "jow"})
+	s := labels.NewSelector().Add(*l)
+	log.Info("Selector", slog.Any("selector", s.String()))
+	labelOptions := informers.WithTweakListOptions(func(options *metav1.ListOptions) {
+		options.LabelSelector = s.String()
+	})
 
-	hash.Reset()
-	hash.Write(f)
-	hashValue = hash.Sum(nil)
+	informerFactory := informers.NewSharedInformerFactoryWithOptions(
+		client,
+		time.Second*30,
+		informers.WithNamespace("default"),
+		labelOptions,
+	)
 
-	fmt.Printf("Data: %s\n Hash: %s\n", f, hex.EncodeToString(hashValue))
+	secretInformer := informerFactory.Core().V1().Secrets().Informer()
+	secretInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			object, ok := obj.(*v1.Secret)
+			if !ok {
+				log.Error("Failed to cast object to secret")
+				return
+			}
+			log.Info(
+				"Secret added",
+				slog.String("secret", object.Name),
+				slog.String("data", fmt.Sprint(object.Data)),
+				slog.String("namespace", object.Namespace),
+			)
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			object, ok := newObj.(*v1.Secret)
+			if !ok {
+				log.Error("Failed to cast object to secret")
+				return
+			}
+			log.Info(
+				"Secret updated",
+				slog.String("secret", object.Name),
+				slog.String("data", fmt.Sprint(object.Data)),
+				slog.String("namespace", object.Namespace),
+			)
+		},
+	})
+
+	informerFactory.Start(ctx.Done())
+	log.Info("Syncing cache")
+	informerFactory.WaitForCacheSync(ctx.Done())
+	log.Info("Cache synced")
+
+}
+
+func init() {
+	w := os.Stderr
+	var handler slog.Handler
+	handler = slog.NewTextHandler(w, nil)
+
+	// If the output is a terminal, enable colors
+	if isatty.IsTerminal(w.Fd()) {
+		handler = tint.NewHandler(w, nil)
+	}
+	slog.SetDefault(slog.New(handler))
 
 }
